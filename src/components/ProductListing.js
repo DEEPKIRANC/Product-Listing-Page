fea1c667a4937e9f3b8e0bd272f8d578e3d90c5f
Deep Kiran <cdeepkiran14@gmail.com>
2021-06-19 16:49:04 +0530

    feat-Sort By Price (low to high and vice versa)

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "../styles/productlist.css";
-
+import { ProductContext } from "../MainProvider";
+import { Product } from "./Product";
 export const ProductListing = () => {
-  return <div className="productList-container"></div>;
+  const [products, setProducts] = useContext(ProductContext);
+  const [productList, setProductList] = useState([]);
+  const [sortPrice, setSortPrice] = useState("");
+  useEffect(() => {
+    setProductList(products);
+  }, [products]);
+  useEffect(() => {
+    if (sortPrice === "") {
+      setProductList(products);
+    } else if (sortPrice === "highToLow") {
+      const sortedProducts = productList.sort((a, b) => a.price - b.price);
+      setProductList(sortedProducts);
+    } else if (sortPrice === "lowToHigh") {
+      const sortedProducts = productList.sort((a, b) => b.price - a.price);
+      setProductList(sortedProducts);
+    }
+  }, [sortPrice]);
+  return (
+    <div className="productList-container">
+      <div className="top-section">
+        <h4>Sort By</h4>
+        <input
+          type="radio"
+          value="lowToHigh"
+          checked={sortPrice === "lowToHigh"}
+          onClick={(e) => setSortPrice(e.target.value)}
+        />
+        Price -- Low to High
+        <input
+          type="radio"
+          value="highToLow"
+          checked={sortPrice === "highToLow"}
+          onClick={(e) => setSortPrice(e.target.value)}
+        />
+        Price -- High to Low
+      </div>
+      <div className="products-section">
+        {productList.length > 0 ? (
+          productList.map((product, index) => {
+            return <Product key={index} product={product} />;
+          })
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </div>
+  );
 };
